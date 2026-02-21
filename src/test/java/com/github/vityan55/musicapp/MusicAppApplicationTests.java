fc09d9e4173fed016ccaab2d214e2d20d4b051d3
Виктор <soyuznikbu@gmail.com>
2026-02-21 17:07:00 +0300

    chore[MUSIC-0]: initial commit

---
+++ src/test/java/com/github/vityan55/musicapp/MusicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vityan55.musicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
