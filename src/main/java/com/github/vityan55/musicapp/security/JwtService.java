e18de7ec1f9158f5ec2c02c484d381c540c0fa82
Виктор <soyuznikbu@gmail.com>
2026-02-21 21:10:42 +0300

    feat(MUSIC-24): added JwtProperties and JwtService

---
+++ src/main/java/com/github/vityan55/musicapp/security/JwtService.java
@@ -0,0 +1,69 @@
+package com.github.vityan55.musicapp.security;
+
+import com.github.vityan55.musicapp.config.JwtProperties;
+import com.github.vityan55.musicapp.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+    private final JwtProperties jwtProperties;
+
+    private Key getSignedKey() {
+        return Keys.hmacShaKeyFor(jwtProperties.getSecret().getBytes());
+    }
+
+    public String generateToken(User user, long expiration) {
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .claim("role", user.getUserRole().name())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSignedKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String generateAccessToken(User user) {
+        return generateToken(user, jwtProperties.getAccessTokenExpiration());
+    }
+
+    public String generateRefreshToken(User user) {
+        return generateToken(user, jwtProperties.getRefreshTokenExpiration());
+    }
+
+    public String extractEmail(String token) {
+        return parseClaims(token).getSubject();
+    }
+
+    public String extractRole(String token) {
+        Object role = parseClaims(token).get("role");
+        return role != null ? role.toString() : null;
+    }
+
+    private Claims parseClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignedKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            parseClaims(token);
+            return true;
+        }
+        catch (JwtException | IllegalArgumentException ex) {
+            return false;
+        }
+    }
+}
