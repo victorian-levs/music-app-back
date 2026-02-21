1d1b5a14579f8bf4e11c8b573c04fd20bb7c4390
Виктор <soyuznikbu@gmail.com>
2026-02-21 23:19:53 +0300

    feat(MUSIC-25): added JwtAuthFilter

--- src/main/java/com/github/vityan55/musicapp/repository/UserRepository.java
+++ src/main/java/com/github/vityan55/musicapp/repository/UserRepository.java
@@ -3,4 +3,8 @@ package com.github.vityan55.musicapp.repository;
 import com.github.vityan55.musicapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
---
+++ src/main/java/com/github/vityan55/musicapp/security/JwtAuthFilter.java
@@ -0,0 +1,63 @@
+package com.github.vityan55.musicapp.security;
+
+import com.github.vityan55.musicapp.entity.User;
+import com.github.vityan55.musicapp.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        String header = request.getHeader("Authorization");
+
+        if (header == null || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = header.substring(7);
+
+        if (!jwtService.validateToken(token)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String email = jwtService.extractEmail(token);
+
+        User user = userRepository.findByEmail(email).orElse(null);
+
+        if (user != null) {
+            UsernamePasswordAuthenticationToken auth =
+                    new UsernamePasswordAuthenticationToken(
+                            user,
+                            null,
+                            List.of(new SimpleGrantedAuthority(user.getUserRole().name()))
+                    );
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
